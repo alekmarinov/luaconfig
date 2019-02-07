72edc0eff48608d37fa4ddebf053e36e18b12ff3
Alexander Marinov <alekmarinov@gmail.com>
2019-02-07 17:32:57 -0500

    release script

--- release.sh
+++ release.sh
@@ -24,9 +24,10 @@ if git status | grep "nothing to commit, working tree clean" > /dev/null; then
     VERSION="$V1.$V2.$V3"
     sed -i "s/tag[[:space:]]*=.*/tag = \"$VERSION\"/" rockspec
     echo "Releasing version $VERSION"
-    # git commit -m "Version incremented to $VERSION"
-    # git log -10 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
-    # git push --tags
+    git commit -m "Version incremented to $VERSION"
+    git tag -a $VERSION -m "Tagged version $VERSION"
+    git log -10 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
+    git push --tags
 else
     echo "You can't release with modified or untracked files since the last commit!"
     echo "The command 'git status' must not show any modified or untracked files."
