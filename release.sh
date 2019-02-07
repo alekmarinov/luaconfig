8b24dac85f4807ebde30bd3dfca6108a5815f343
Alexander Marinov <alekmarinov@gmail.com>
2019-02-07 17:55:33 -0500

    release.sh version parse fix

--- release.sh
+++ release.sh
@@ -3,28 +3,29 @@
 if git status | grep "nothing to commit, working tree clean" > /dev/null; then
     VERSION=`git describe`
 
-    # parse git version
-    VSHA=${VERSION/[^-]*\-/}
-    V1=${VERSION/\.[0-9]*/}
-    V3=${VERSION/[0-9]*\./}
-    V2=${V3/-*/}
-    V3=${V3#$V2-}
-    V3=${V3/-*/}
+    VERSION=(${VERSION//[\.\-]/ })
+    V1=${VERSION[0]}
+    V2=${VERSION[1]}
+    V3=${VERSION[2]}
+    V4=${VERSION[3]}
 
     # increment release
     release_type=$1
     if [ "$release_type" == "major" ]; then
         V1=$((V1+1))
+        V2=0
+        V3=0
     elif [ "$release_type" == "minor" ]; then
         V2=$((V2+1))
+        V3=0
     else
         V3=$((V3+1))
     fi
 
     VERSION="$V1.$V2.$V3"
+    echo "Releasing version $VERSION"
     sed -i "s/version[[:space:]]*=.*/version = \"$VERSION-0\"/" rockspec
     sed -i "s/tag[[:space:]]*=.*/tag = \"$VERSION\"/" rockspec
-    echo "Releasing version $VERSION"
     git commit -a -m "Version incremented to $VERSION"
     git tag -a $VERSION -m "Tagged version $VERSION"
     git log -10 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative
